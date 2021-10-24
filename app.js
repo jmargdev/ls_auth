3280fbf97d690ea721140d8feefcd98479059fd5
John Margaronis <john@johnmargaronis.com>
2021-10-24 13:54:54 -0500

    Added error/success handling

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const path = require('path');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
@@ -24,6 +26,25 @@ app.set('view engine', 'ejs');
 
 // Bodyparser
 app.use(express.urlencoded({ extended: false }));
+
+// Express Session
+app.use(
+  session({
+    secret: 'canYoUkeepAseCr3t123123',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Connect flash
+app.use(flash());
+
+// Global vars
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  next();
+});
 
 // Routes
 app.use('/', require('./routes/index.js'));
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+// User model
+const User = require('../models/User');
 
 // Login page
 router.get('/login', (req, res) => res.render('login'));
@@ -34,7 +38,45 @@ router.post('/register', (req, res) => {
       password2
     });
   } else {
-    res.send('passed');
+    // Validation passed
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        // User exists
+        errors.push({ msg: 'A user with that email already exists' });
+        res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          password2
+        });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password
+        });
+        // Hash password
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            // Set password to hash password
+            newUser.password = hash;
+            // Save user
+            newUser
+              .save()
+              .then(user => {
+                req.flash(
+                  'success_msg',
+                  'Account has been created successfully. Please use the login form below to login.'
+                );
+                res.redirect('/users/login');
+              })
+              .catch(err => console.log(err));
+          })
+        );
+      }
+    });
   }
 });
 
