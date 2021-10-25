6fab958486fbab40332bc517a74cbc0ca2314537
John Margaronis <john@johnmargaronis.com>
2021-10-25 16:11:18 -0500

    Added timestamps to user schema

--- models/User.js
+++ models/User.js
@@ -1,23 +1,30 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    },
+    createdAt: Number,
+    updatedAt: Number
   },
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
+  {
+    timestamps: true
   }
-});
+);
 
 const User = mongoose.model('User', UserSchema);
 
