b5bcf7577b5062e16ec50a95541c34b1e8a0e44b
John Margaronis <john@johnmargaronis.com>
2021-10-25 16:16:57 -0500

    Created passport logic for authentication

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const mongoose = require('mongoose');
 const path = require('path');
 const flash = require('connect-flash');
 const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
+
+// Passport configs
+require('./config/passport')(passport);
 
 // Database config
 const db = require('./config/keys').mongoURI;
@@ -36,6 +40,10 @@ app.use(
   })
 );
 
+// Passport initialization
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Connect flash
 app.use(flash());
 
@@ -43,6 +51,7 @@ app.use(flash());
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
   next();
 });
 
